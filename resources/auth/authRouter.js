4d8907f58f589673bcabe5e54ce88b1f1e78e553
LD <LDinho@users.noreply.github.com>
2019-07-17 03:29:38 -0400

    add login endpoint to auth router

--- resources/auth/authRouter.js
+++ resources/auth/authRouter.js
@@ -59,4 +59,59 @@ router.post('/register', async (req, res) => {
 
 })
 
+/*
+@POST: login user
+@PARAMS: req.body as { email, password }
+@ROUTE: "/api/auth"
+*/
+router.post('/login', async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+
+    if (!email || !password) {
+      return res.status(400)
+        .json({
+          errorMessage: "email or password missing."
+        });
+    }
+
+    const user = await getUserBy({ email });
+
+    if (!user) {
+      return res.status(400)
+        .json({
+          message: `user not found`
+        });
+    }
+
+    const isValidPassword =
+      bcrypt.compareSync(password, user.password);
+
+    if (!isValidPassword) {
+      return res.status(401)
+        .json({
+          message: 'Invalid Credentials'
+        });
+    } else {
+
+      // use of the jwt library
+      const token = generateToken(user);
+
+      return res.status(200)
+        .json({
+          message: `Welcome ${user.email}!`,
+          authToken: token,
+        });
+    }
+  }
+  catch (err) {
+    res.status(500)
+      .json({
+        err: err.message,
+        error: `Server error`
+      })
+  }
+})
+
 module.exports = router;
